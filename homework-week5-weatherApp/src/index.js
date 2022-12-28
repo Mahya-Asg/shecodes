a6544ee8738f5ed3ded1e5fc2d110b2009d829aa
maha.asg <maha.asg@users.noreply.github.com>
2022-12-28 12:15:08 +0330

    updated js file - functions are cleaned and organized- adjust project folder name
    instead of two functions for each buttom api call, now both use the same function "showWeatherInfo"
    the app doesn't ask for location permission on every reload instead it only asks when the currernt button is clicked

--- homework-week5-weatherApp/src/index.js
+++ homework-week5-weatherApp/src/index.js
@@ -24,20 +24,26 @@ function formatDate(date) {
 
 	return `${day} ${hours}:${minutes}`;
 }
-
 let dateElement = document.querySelector("#date");
 // console.log(dateElement);
 let today = new Date();
 dateElement.innerHTML = formatDate(today);
 
 // Feature #2
-// use weather api to update searched city weather info real time
+// use weather api to update searched city weather info in real time
 
-function cityWeather(response) {
+function showWeatherInfo(response) {
 	console.log(response.data);
-	let temperature = Math.round(response.data.main.temp);
-	// console.log(`the temprature is: ${temperature}`);
 
+	let cityElement = document.querySelector("#city");
+	cityElement.innerHTML = response.data.name;
+
+	// undone feature
+	// how to clear input form value when clicking on the current button?
+	// let currentInput = document.querySelector("#city-input");
+	// currentInput.value.innerHTML = response.data.name;
+
+	let temperature = Math.round(response.data.main.temp);
 	let tempElement = document.querySelector("#temperature");
 	tempElement.innerHTML = temperature;
 
@@ -53,50 +59,23 @@ function cityWeather(response) {
 	windSpeed.innerHTML = windSpeedRounded;
 }
 
-function searchCity(event) {
-	event.preventDefault();
-	let cityElement = document.querySelector("#city");
-	let cityInput = document.querySelector("#city-input");
-	cityElement.innerHTML = cityInput.value;
-	console.log(cityInput.value);
-	let city = cityInput.value;
+function searchCity(city) {
 	let units = "metric";
 	let url = "https://api.openweathermap.org/data/2.5/weather?q=";
 	let apiKey = "63214c4281922e3bb72fdf12dada7734";
-	axios.get(`${url}${city}&units=${units}&appid=${apiKey}`).then(cityWeather);
+	axios
+		.get(`${url}${city}&units=${units}&appid=${apiKey}`)
+		.then(showWeatherInfo);
 }
 
-let searchBtn = document.querySelector("#search-btn");
-searchBtn.addEventListener("click", searchCity);
+function handleSubmit(event) {
+	event.preventDefault();
+	let city = document.querySelector("#city-input").value;
+	searchCity(city);
+}
 
 // Bonus Function
-
-function showCurrentWeather(response) {
-	console.log("showCurrentWeather(response):");
-	// console.log(response.data);
-	// console.log(response.data.name);
-	// let h1 = document.querySelector("h1");
-	// let temperature = Math.round(response.data.main.temp);
-	// h1.innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
-	let cityElement = document.querySelector("#city");
-	cityElement.innerHTML = response.data.name;
-
-	let temprature = Math.round(response.data.main.temp);
-	let tempElement = document.querySelector("#temperature");
-	tempElement.innerHTML = temprature;
-
-	let weatherDescription = document.querySelector("#description");
-	weatherDescription.innerHTML = response.data.weather[0].main;
-
-	let humidity = document.querySelector("#humidity");
-	humidity.innerHTML = response.data.main.humidity;
-
-	let windSpeed = document.querySelector("#wind");
-	windSpeedRounded = Math.round(response.data.wind.speed);
-	windSpeed.innerHTML = windSpeedRounded;
-}
-
-function currentLocationWeather(position) {
+function getCurrentLocation(position) {
 	// console.log(position.coords.latitude);
 	// console.log(position.coords.longitude);
 	console.log(position);
@@ -106,10 +85,18 @@ function currentLocationWeather(position) {
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
 	// let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={${latitude}}&lon={${longitude}}&exclude={minutely,alert}&appid={${apiKey}}`;
 
-	axios.get(apiUrl).then(showCurrentWeather);
+	axios.get(apiUrl).then(showWeatherInfo);
 }
 
-navigator.geolocation.getCurrentPosition(currentLocationWeather);
+function navigateCurrentLocation(event) {
+	event.preventDefault();
+	navigator.geolocation.getCurrentPosition(getCurrentLocation);
+}
+
+// navigator.geolocation.getCurrentPosition(currentLocationWeather);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
 
 let currentBtn = document.querySelector("#current-btn");
-currentBtn.addEventListener("click", currentLocationWeather);
+currentBtn.addEventListener("click", navigateCurrentLocation);
